2a5f502415e5008cdde6df3e293d56a0cd3fe83b
Chritsian LaForest <christianlaforest45@gmail.com>
2024-05-22 15:04:26 -0400

    model size changed

--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ import postprocess_yolov4 as post
 from PIL import Image
 import onnxruntime as rt
 
-input_size = 416
-original_image = cv2.imread("../testing/kite.jpg")
+input_size = 640
+original_image = cv2.imread("kite.jpg")
 original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 original_image_size = original_image.shape[:2]
 image_data = pre.image_preprocess(np.copy(original_image), [input_size, input_size])
 image_data = image_data[np.newaxis, ...].astype(np.float32)
 print("Preprocessed image shape:", image_data.shape)  # shape of the preprocessed input
 
-sess = rt.InferenceSession("yolov4.onnx")
+sess = rt.InferenceSession("/opt/detect/model.onnx")
 
 output_name = sess.get_outputs()[0].name
 input_name = sess.get_inputs()[0].name

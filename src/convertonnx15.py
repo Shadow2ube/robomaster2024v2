fa7c9331ea6d18793a9e7a45ec5504855b4410c5
Chritsian LaForest <christianlaforest45@gmail.com>
2024-06-03 19:36:43 -0400

    added some debug output

--- src/convertonnx15.py
+++ src/convertonnx15.py
@@ -3,7 +3,14 @@ import sys
 from onnx import version_converter
 
 model_path = sys.argv[1]
+print('loading model from:', model_path, '...', end='')
 original_model = onnx.load_model(model_path)
+print('done')
 
+print('converting model...', end='')
 converted_model = version_converter.convert_version(original_model, 15)
+print('done')
+
+print('saving to: ', sys.argv[2], '...', end='')
 onnx.save(converted_model, sys.argv[2])
+print('done')
